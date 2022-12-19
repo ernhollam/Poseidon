95420619c1b62eb65bedd8f15afa0b4addacbee3
e.marinasy <ernholla.marinasy@gmail.com>
2022-12-19 18:57:35 +0100

    Add user and login controller tests

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/LoginControllerTest.java
@@ -0,0 +1,48 @@
+package com.nnk.springboot.controllers;
+
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.repositories.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(controllers = LoginController.class, secure = false)
+@RunWith(SpringRunner.class)
+public class LoginControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserRepository userRepository;
+
+    @Test
+    public void testGetAllUserArticles() throws Exception {
+        List<User> expectedList = new ArrayList<>();
+        when(userRepository.findAll()).thenReturn(expectedList);
+        mockMvc.perform(get("/app/secure/article-details"))
+               .andExpect(view().name("user/list"))
+               .andExpect(model().attribute("users", expectedList));
+    }
+
+    @Test
+    @WithMockUser
+    public void testError() throws Exception {
+        mockMvc.perform(get("/app/error"))
+               .andExpect(view().name("403"))
+               .andExpect(model().attributeExists("errorMsg"))
+               .andExpect(model().attribute("errorMsg", "You are not authorized for the requested data."));
+    }
+}
---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/UserControllerTest.java
@@ -0,0 +1,152 @@
+package com.nnk.springboot.controllers;
+
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = UserController.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Import(value = IllegalArgumentException.class)
+public class UserControllerTest {
+    @Autowired
+    MockMvc               mockMvc;
+    @MockBean
+    UserRepository        repository;
+    @MockBean
+    BCryptPasswordEncoder encoder;
+
+    private User       user;
+    private List<User> users;
+    private String encodedPassword;
+
+    @BeforeAll
+    public void setUp() {
+        user = new User();
+        users = Collections.singletonList(user);
+        encodedPassword = "-*/PASSw0rd!";
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Return list of users")
+    public void homeTest() throws Exception {
+        when(repository.findAll()).thenReturn(users);
+
+        mockMvc.perform(get("/user/list"))
+               .andExpect(status().is2xxSuccessful())
+               .andExpect(model().attributeExists("users"));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Show add user form")
+    public void addUserFormTest() throws Exception {
+        mockMvc.perform(get("/user/add"))
+               .andExpect(status().is2xxSuccessful())
+               .andExpect(view().name("user/add"));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Add new user successful")
+    public void validateTest() throws Exception {
+        when(encoder.encode(anyString())).thenReturn(encodedPassword);
+        when(repository.save(user)).thenReturn(user);
+
+        mockMvc.perform(post("/user/validate")
+                                .with(csrf().asHeader())
+                                .param("fullname", "My Full Name")
+                                .param("username", "username")
+                                .param("password", encodedPassword)
+                                .param("role", "ADMIN"))
+               .andExpect(status().is3xxRedirection())
+               .andExpect(view().name("redirect:/user/list"));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Show update form successful")
+    public void showUpdateFormIsSuccessful() throws Exception {
+        when(repository.findById(any(Integer.class))).thenReturn(Optional.of(user));
+
+
+        mockMvc.perform(get("/user/update/{id}", "1"))
+               .andExpect(status().is2xxSuccessful())
+               .andExpect(model().attributeExists("user"))
+               .andExpect(view().name("user/update"));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Show update form failed")
+    public void showUpdateFormFails() throws Exception {
+        when(repository.findById(3)).thenReturn(Optional.empty());
+        assertThrows(Exception.class, () -> mockMvc.perform(get("/user/update/{id}", "3")));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Update user successful")
+    public void updateBidTest() throws Exception {
+        when(encoder.encode(anyString())).thenReturn("password");
+        when(repository.save(user)).thenReturn(user);
+        when(repository.findAll()).thenReturn(users);
+
+        mockMvc.perform(post("/user/update/{id}", "1")
+                                .with(csrf().asHeader())
+                                .param("fullname", "My New Full Name")
+                                .param("username", "username")
+                                .param("password", encodedPassword)
+                                .param("role", "ADMIN"))
+               .andExpect(status().is3xxRedirection())
+               .andExpect(view().name("redirect:/user/list"));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Delete user successful")
+    public void deleteBidIsSuccessful() throws Exception {
+        when(repository.findById(any(Integer.class))).thenReturn(Optional.of(user));
+        when(repository.findAll()).thenReturn(users);
+
+        mockMvc.perform(get("/user/delete/{id}", "1"))
+               .andExpect(status().is3xxRedirection())
+               .andExpect(view().name("redirect:/user/list"));
+    }
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    @DisplayName("Delete user failed")
+    public void deleteBidFailed() throws Exception {
+        when(repository.findById(any(Integer.class))).thenReturn(Optional.empty());
+        assertThrows(Exception.class, () -> mockMvc.perform(get("/user/delete/{id}", "1")));
+    }
+}
