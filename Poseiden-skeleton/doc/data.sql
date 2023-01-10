6b741293ae0470aa2e61cfe57eaabd00bb44b860
e.marinasy <ernholla.marinasy@lomaco.fr>
2023-01-10 16:11:50 +0100

    Update data.sql to make IT tests pass

--- Poseiden-skeleton/doc/data.sql
+++ Poseiden-skeleton/doc/data.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE demo;
+CREATE SCHEMA IF NOT EXISTS demo;
 use demo;
 
 CREATE TABLE BidList (
@@ -96,6 +96,3 @@ CREATE TABLE Users (
 
   PRIMARY KEY (Id)
 );
-
-insert into Users(fullname, username, password, role) values("Administrator", "admin", "$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa", "ADMIN");
-insert into Users(fullname, username, password, role) values("User", "user", "$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa", "USER");
